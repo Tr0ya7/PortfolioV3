265287c90135efee7ca104a7ea9a2904e897cf51
Luiz Henrique <136819845+Tr0ya7@users.noreply.github.com>
2023-12-29 14:02:28 -0300

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link 
           href="https://fonts.googleapis.com/css2?family=Handjet&family=Press+Start+2P&family=Saira&display=swap" 
           rel="stylesheet" 
